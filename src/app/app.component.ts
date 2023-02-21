e01ef024ae1bf5385381e4e408cb7a02c20e225a
Luc Jaubert <lucjaubert@gmail.com>
2023-02-21 10:04:44 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Developer } from './models/developer.model';
+import { Skill } from './models/skill.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AtelierRouter';
+  developer: Developer;
+
+  constructor() {
+    this.developer = new Developer(
+      'Jaubert',
+      'Luc',
+      39,
+      'Homme',
+      'Je suis un développeur passionné qui aime résoudre des problèmes et créer des applications innovantes.',
+      [
+        new Skill(
+          'Angular',
+          'https://angular.io/assets/images/logos/angular/angular.svg',
+          'https://angular.io/'
+        ),
+        new Skill(
+          'React',
+          'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+          'https://reactjs.org/'
+        )
+      ]
+    );
+  }
 }
